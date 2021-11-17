7d406a29071adda30ecbe0ae7bf1c1c5e3d02a97
Andrew <andrewsmith1025@gmail.com>
2021-11-16 19:14:10 -0500

    single cycle mips suggestion

--- single_cycle_mips.sv
+++ single_cycle_mips.sv
@@ -2,5 +2,9 @@ module single_cycle_mips (
     input logic clk, // clock signal
     input logic rst_n // active-low reset signal used for initialization
 );
-    
+    logic [31:0] pc, instr, readdata;
+    // instantiate processor and memories
+    mips mips(clk, reset, pc, instr, memwrite, dataadr,writedata, readdata);
+    imem imem(pc[7:2], instr);
+    dmem dmem(clk, memwrite, dataadr, writedata, readdata);
 endmodule
