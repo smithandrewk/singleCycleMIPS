6dd7d456a9ef14d36f1a76f5367885716e13f8d5
Andrew <andrewsmith1025@gmail.com>
2021-11-16 19:29:52 -0500

    add ports

--- single_cycle_mips.sv
+++ single_cycle_mips.sv
@@ -3,8 +3,12 @@ module single_cycle_mips (
     input logic rst_n // active-low reset signal used for initialization
 );
     logic [31:0] pc, instr, readdata;
+    logic [31:0] writedata, dataadr;
+    logic       memwrite;
+    logic [31:0] pc, instr, readdata;
+    
     // instantiate processor and memories
-    mips mips(clk, reset, pc, instr, memwrite, dataadr,writedata, readdata);
+    mips mips(clk, rst_n, pc, instr, memwrite, dataadr,writedata, readdata);
     imem imem(pc[7:2], instr);
     dmem dmem(clk, memwrite, dataadr, writedata, readdata);
 endmodule
